78368860f485ff4d741fd0bd7e0fb2b3019b1862
qianhoucai <qianhoucai@gerrit.cmss.com>
2020-08-04 18:04:12 +0800

    代码优化，service分为接口和实现

--- src/main/java/com/tsien/contentcenter/service/content/ShareService.java
+++ src/main/java/com/tsien/contentcenter/service/content/ShareService.java
@@ -1,14 +1,6 @@
 package com.tsien.contentcenter.service.content;
 
-import com.tsien.contentcenter.dao.content.ShareMapper;
 import com.tsien.contentcenter.domain.dto.content.ShareDTO;
-import com.tsien.contentcenter.domain.dto.user.UserDTO;
-import com.tsien.contentcenter.domain.model.content.Share;
-import com.tsien.contentcenter.feignclient.UserCenterFeignClient;
-import org.springframework.beans.BeanUtils;
-import org.springframework.stereotype.Service;
-
-import javax.annotation.Resource;
 
 /**
  * Created with IntelliJ IDEA.
@@ -18,38 +10,13 @@ import javax.annotation.Resource;
  * @date 2020/7/19 0019 2:58
  */
 
-@Service
-public class ShareService {
-
-    @Resource
-    private ShareMapper shareMapper;
-
-    @Resource
-    private UserCenterFeignClient userCenterFeignClient;
+public interface ShareService {
 
     /**
-     * 通过ID获取分享详细
+     * 根据用户ID查找分享
      *
      * @param id id
-     * @return Share
+     * @return ShareDTO
      */
-    public ShareDTO findById(Integer id) {
-
-        // 获取发布详情
-        Share share = shareMapper.selectByPrimaryKey(id);
-
-        // 发布人ID
-        Integer userId = share.getUserId();
-
-        // 使用OpenFeign 获取用户中心UserDTO
-        UserDTO userDTO = userCenterFeignClient.findById(userId);
-
-        // 消息装配
-        ShareDTO shareDTO = new ShareDTO();
-        BeanUtils.copyProperties(share, shareDTO);
-        shareDTO.setWxNickname(userDTO != null ? userDTO.getWxNickname() : null);
-
-        return shareDTO;
-
-    }
+    ShareDTO findById(Integer id);
 }
---
+++ src/main/java/com/tsien/contentcenter/service/content/impl/ShareServiceImpl.java
@@ -0,0 +1,56 @@
+package com.tsien.contentcenter.service.content.impl;
+
+import com.tsien.contentcenter.dao.content.ShareMapper;
+import com.tsien.contentcenter.domain.dto.content.ShareDTO;
+import com.tsien.contentcenter.domain.dto.user.UserDTO;
+import com.tsien.contentcenter.domain.model.content.Share;
+import com.tsien.contentcenter.feignclient.UserCenterFeignClient;
+import com.tsien.contentcenter.service.content.ShareService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author tsien
+ * @version 1.0.0
+ * @date 2020/8/4 0004 17:09
+ */
+
+@Service
+public class ShareServiceImpl implements ShareService {
+
+    @Resource
+    private ShareMapper shareMapper;
+
+    @Resource
+    private UserCenterFeignClient userCenterFeignClient;
+
+    /**
+     * 通过ID获取分享详细
+     *
+     * @param id id
+     * @return Share
+     */
+    @Override
+    public ShareDTO findById(Integer id) {
+
+        // 获取发布详情
+        Share share = shareMapper.selectByPrimaryKey(id);
+
+        // 发布人ID
+        Integer userId = share.getUserId();
+
+        // 使用OpenFeign 获取用户中心UserDTO
+        UserDTO userDTO = userCenterFeignClient.findById(userId);
+
+        // 消息装配
+        ShareDTO shareDTO = new ShareDTO();
+        BeanUtils.copyProperties(share, shareDTO);
+        shareDTO.setWxNickname(userDTO != null ? userDTO.getWxNickname() : null);
+
+        return shareDTO;
+    }
+}
